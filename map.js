9afed70700a59b7ad46327c734220cc56d12c98c
Calvin Metcalf <calvin.metcalf@state.ma.us>
2012-05-02 14:33:36 +0000

    first commit

---
+++ map.js
@@ -0,0 +1,14 @@
+var m;
+var zoom = 8;
+var center = new google.maps.LatLng(42.04113400940814,-71.795654296875);
+$(function() { map();
+});
+
+function map(){
+            m = new google.maps.Map(document.getElementById('map'), {
+      center: center,
+      zoom: zoom,
+      mapTypeId: 'roadmap'
+    });
+    
+}
